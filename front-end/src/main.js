d0a1213f0f37821c710c6b062eb90ff5813dc82d
chardensoft <chardensoft@gmail.com>
2021-07-15 16:34:55 -0600

    analytics

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -2,16 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import titleMixin from './mixins/titleMixin'
-import VueAnalytics from 'vue-analytics';
+import VueAnalytics from 'vue-analytics'
 
 
 Vue.config.productionTip = false
 Vue.mixin(titleMixin)
-//and then use it in main.js
 Vue.use(VueAnalytics, {
     id: 'UA-278706947-1',
     router
-}
+})
 
 new Vue({
   router,
