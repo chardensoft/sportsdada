c184fed7c9c406d05a392e5816d6508774439fd3
chardensoft <chardensoft@gmail.com>
2021-08-02 23:07:57 -0600

    home page

---
+++ front-end/src/components/HomeHeader.vue
@@ -0,0 +1,168 @@
+<template>
+  <div class="full-header">
+    <div class="header">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <!-- <li class="nav-item">
+              <a class="nav-link" href="html/apply.html">Apply</a>
+            </li> -->
+            <li class="nav-item">
+              <router-link class ="header-link" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class ="header-link" to="/Content">Content</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class ="header-link" to="/About">About</router-link>
+            </li>
+            <li class="nav-item">
+              <a class ="header-link wreport-link" href="https://thewoodreportxc.com/" target="_blank">
+                  <img class = "normal-wreport-img" src="@/assets/wreport.png" /><p>WR</p></a>
+            </li>
+            <li class="nav-item">
+              <router-link class ="header-link powder-link" to="/PowderStats">
+                  <img class = "normal-powder-img" src="@/assets/powder.png" /><p>BPL</p></router-link>
+            </li>
+            <!-- <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Products
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="">Fake Plants</a>
+                <a class="dropdown-item" href="">Real Plants</a>
+                <a class="dropdown-item" href="">Accessories</a>
+              </div>
+            </li> -->
+
+          </ul>
+        </div>
+        <div class="header_buttons">
+          <button class = "" onclick="location.href = 'https://venmo.com/charden'">Donate</button>
+          <button class = "" onclick="location.href = 'https://www.instagram.com/sports.dada/'">Follow</button>
+        </div>
+        <img class="normal-logo" src="../assets/logo.png" />
+
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeHeader',
+  components: {
+  }
+}
+</script>
+
+<style>
+p {
+  margin: 0;
+}
+.normal-logo {
+  width: 400px;
+}
+.normal-powder-img {
+  width: 35px;
+}
+.normal-wreport-img {
+  width: 35px;
+}
+.powder-link,
+.powder-link:active {
+  color: #030303 !important;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.wreport-link,
+.wreport-link:active {
+  color: #F3B23D !important;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.powder-link:hover,
+.powder-link:hover:active {
+  color: #616161 !important;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.wreport-link:hover,
+.wreport-link:hover:active {
+  color: #0F4C75 !important;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.header_buttons {
+  margin-right: 10px;
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+}
+button:active, button {
+  cursor: pointer;
+  margin: 5px;
+  border-radius: 5px;
+  border: solid #83C4E7 1px;
+  background-color: #83C4E7;
+  color: black;
+}
+button:hover,
+button:hover:active {
+  cursor: pointer;
+  margin: 5px;
+  border-radius: 5px;
+  border: solid #83C4E7 1px;
+  background-color: #F8F9FA;
+  color: #CE663D;
+}
+.header {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+  /*color: #CE663D; <- orange*/
+  /*color: #83C4E7; <- light blue*/
+  /*color: #4670B4; <- dark blue */
+
+.nav-item {
+  font-size: 25px;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+}
+
+.header-link,
+.header-link:active {
+  padding: 7px;
+  color: #CE663D;
+}
+
+.header-link:hover,
+.header-link:active:hover {
+  color: #4670B4;
+}
+
+/* Mobile Styles */
+@media only screen and (max-width: 991px) {
+  .normal-logo {
+    width: 200px;
+  }
+  .normal-powder-img {
+    width: 35px;
+  }
+}
+
+
+</style>
--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,23 +1,28 @@
 <template>
   <div class="home">
-    <Header />
-    <div>
-      <h1>What is Sports Dada?</h1>
-
+    <HomeHeader />
+    <div class="featured">
+      <a class="featured-link" href="https://sportsdadas.blogspot.com/2021/07/sports-dadas-powder-league-mvp-ladder.html" target="_blank">
+      <div class="featured-content">
+        <h1 class ="featured-header">Click Here! Featured Article: Powder League MVP Ladder</h1>
+      </div></a>
+    </div>
+    <div class="article-list">
+      <h3 class="">Article 1</h3>
     </div>
     <Footer />
   </div>
 </template>
 
 <script>
-import Header from '@/components/Header.vue';
+import HomeHeader from '@/components/HomeHeader.vue';
 import Footer from '@/components/Footer.vue';
 
 export default {
   name: 'Home',
   title: 'Sports Dada',
   components: {
-    Header,
+    HomeHeader,
     Footer,
   }
 }
